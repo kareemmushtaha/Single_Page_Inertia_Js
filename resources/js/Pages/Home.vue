0447c9a014c057cc1b54c1034a210e8855fbd39f
kareemmushtaha <1142000k##$#>
2022-02-28 20:21:38 +0200

    2/28/2022/V1

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,16 @@
+<template>
+    <h1> hello {{ name }} </h1>
+    <ul>
+        <li v-for="framework of frameworks" v-text="framework"> </li>
+    </ul>
+ </template>
+
+
+<script>
+export default {
+    props: {
+        name:String,
+        frameworks:Array,
+    }
+};
+</script>
